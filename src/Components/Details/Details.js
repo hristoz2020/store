db5ee6a60e662f2b3a367258ea7a0c01c591f1d8
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-02-10 22:35:22 +0200

    add loader

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import * as ProductService from "../../Services/ProductServices";
+import Loader from "../Loader/Loader";
 
 const Details = () => {
     const [loading, setLoading] = useState(true);
@@ -24,16 +25,21 @@ const Details = () => {
 
 	return (
 		<div className="details-card">
-			{loading && <p>Loading...</p>} 
+			{loading 
+				? <Loader />
+				: <>
+					<img className="details-card-img left" src={product.image} alt="product" />
+					<div className="details-card-info right">
+						<h3 className="details-card-title">Title: {product.title}</h3>
+						<p className="details-card-category">Category: {product.category}</p>
+						<p className="details-card-description">Description: {product.description}</p>
+						<p className="details-card-price">Price: {product.price}BGN</p>
+						<p className="">Rating: {product?.rating?.rate}</p>
+					</div>
+				</>
+			} 
 			{error && <p>Error!!</p>}
-			<img className="details-card-img left" src={product.image} alt="product" />
-            <div className="details-card-info right">
-                <h3 className="details-card-title">Title: {product.title}</h3>
-                <p className="details-card-category">Category: {product.category}</p>
-                <p className="details-card-description">Description: {product.description}</p>
-                <p className="details-card-price">Price: {product.price}BGN</p>
-                <p className="">Rating: {product?.rating?.rate}</p>
-            </div>
+			
 		</div>
 	);
 };
--- src/Components/Pages/AllProducts/AllProducts.js
+++ src/Components/Pages/AllProducts/AllProducts.js
@@ -4,25 +4,23 @@ import CardContainer from "../../CardContainer/CardContainer";
 
 const AllProducts = () => {
 	const [products, setProducts] = useState([]);
-	const [btnText, setBtnText] = useState('desc');
+	const [sortBy, setSortBy] = useState('desc');
+	console.log(products);
+	const checkSortBy = sortBy === 'desc' ? 'asc' : 'desc';
 
 	useEffect(() => {
-		ProductServices.getAscOeDescProducts(btnText)
+		ProductServices.getAscOeDescProducts(sortBy)
 			.then(result => setProducts(result))
 			.catch(err => {
 				console.log(err);
 			})
-	}, [btnText]);
-
-	function onClickedBtn(e) {
-		setBtnText('asc');
-	}
+	}, [sortBy]);
 
 	return (
 		<div className="all-products-page">
 			<h1>All Products</h1>
 			Sort by:
-			<button onClick={onClickedBtn}>{btnText}</button>
+			<button onClick={() => setSortBy(checkSortBy)}>{checkSortBy}</button>
 			<ul className="card-list">
 				{products.map((x) => (
 					<CardContainer key={x.id} product={x} button={x} />
