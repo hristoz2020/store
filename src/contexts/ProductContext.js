9f4c6dfe1c938d3c5c2dbd343ae600f26163ceb2
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-04-21 13:07:37 +0300

    get limitProducts in home page

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,11 +1,29 @@
 import { createContext, useEffect, useState, useMemo } from "react";
+import { getLimitProducts } from "../services/productServices";
 
 export const ProductContext = createContext();
 
 export const AllProductsContext = ({ children }) => {
+	const [limitProducts, setLimitProducts] = useState(
+		localStorage.getItem("limitProducts")
+			? JSON.parse(localStorage.getItem("limitProducts"))
+			: []
+	);
+
+	useEffect(() => {
+		getLimitProducts().then((res) => {
+			setLimitProducts(res);
+			localStorage.setItem("limitProducts", JSON.stringify(res));
+		});
+	}, []);
+
+	const value = useMemo(
+		() => ({ limitProducts }),
+		[limitProducts]
+	);
 
 	return (
-		<ProductContext.Provider>
+		<ProductContext.Provider value={value}>
 			{children}
 		</ProductContext.Provider>
 	);
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,39 +1,22 @@
-import { useState, useEffect } from "react";
-import { getLimitProducts } from "../../services/productServices";
+import { useContext } from "react";
+import { ProductContext } from "../../contexts/ProductContext";
 import CardContainer from "../../components/CardContainer/CardContainer";
 import Loader from "../../components/Loader/Loader";
 
-const Home = ({user, cart}) => {
-	const [products, setProducts] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		getLimitProducts()
-			.then((result) => {
-				setIsLoading(false);
-				setProducts(result);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
-
+const Home = ({ user, cart }) => {
+	const { limitProducts } = useContext(ProductContext);
 
 	return (
 		<>
 			<div className="home-page">
-				{isLoading ? (
-					<Loader />
-				) : (
-					<>
-						<h1>Wellcome {user} to store!</h1>
-						<ul className="card-list">
-							{products.map((x) => (
-								<CardContainer key={x.id} product={x} cart={cart} />
-							))}
-						</ul>
-					</>
-				)}
+				{limitProducts.length < 1 && <Loader />}
+				<h1>Wellcome {user} to store!</h1>
+				<ul className="card-list">
+					{limitProducts.length > 0 &&
+						limitProducts.map((x) => (
+							<CardContainer key={x.id} product={x} cart={cart} />
+						))}
+				</ul>
 			</div>
 		</>
 	);
