cafd9598a4e3e6084c2767e44683f4ffb5def6b2
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-04-24 13:30:19 +0300

    create payment form

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -6,7 +6,150 @@ const Order = () => {
 	const totalPrice = data.toFixed(2);
 
 	return (
-		<h1>{totalPrice}</h1>
+		<div className="wrapper">
+			<h2>Payment Form</h2>
+			<form>
+				<h4>Account</h4>
+				<div className="input-group">
+					<div className="input-box">
+						<input
+							type="text"
+							placeholder="Full Name"
+							required
+							className="name"
+						/>
+						<i className="fa fa-user icon"></i>
+					</div>
+					<div className="input-box">
+						<input
+							type="text"
+							placeholder="Nick Name"
+							required
+							className="name"
+						/>
+						<i className="fa fa-user icon"></i>
+					</div>
+				</div>
+				<div className="input-group">
+					<div className="input-box">
+						<input
+							type="email"
+							placeholder="Email Adress"
+							required
+							className="name"
+						/>
+						<i className="fa fa-envelope icon"></i>
+					</div>
+				</div>
+				<div className="input-group">
+					<div className="input-box">
+						<h4> Date of Birth</h4>
+						<input type="number" placeholder="DD" className="dob" />
+						<input type="number" placeholder="MM" className="dob" />
+						<input
+							type="number"
+							placeholder="YYYY"
+							className="dob"
+						/>
+					</div>
+					<div className="input-box">
+						<h4> Gender</h4>
+						<input
+							type="radio"
+							id="b1"
+							name="gendar"
+							defaultChecked
+							className="radio"
+						/>
+						<label htmlFor="b1">Male</label>
+						<input
+							type="radio"
+							id="b2"
+							name="gendar"
+							className="radio"
+						/>
+						<label htmlFor="b2">Female</label>
+					</div>
+				</div>
+				<div className="input-group">
+					<div className="input-box">
+						<h4>Payment Details</h4>
+						<input
+							type="radio"
+							name="pay"
+							id="bc1"
+							defaultChecked
+							className="radio"
+						/>
+						<label htmlFor="bc1">
+							<span className="card-type">
+								<i className="fa-brands fa-cc-visa"></i> Credit
+								Card
+							</span>
+						</label>
+						<input
+							type="radio"
+							name="pay"
+							id="bc2"
+							className="radio"
+						/>
+						<label htmlFor="bc2">
+							<span className="card-type">
+								<i className="fa-brands fa-cc-paypal"></i>{" "}
+								Paypal
+							</span>
+						</label>
+					</div>
+				</div>
+				<div className="input-group">
+					<div className="input-box">
+						<input
+							type="tel"
+							placeholder="Card Number"
+							required
+							className="name"
+						/>
+						<i className="fa fa-credit-card icon"></i>
+					</div>
+				</div>
+				<div className="input-group">
+					<div className="input-box">
+						<input
+							type="tel"
+							placeholder="Card CVC"
+							required
+							className="name"
+						/>
+						<i className="fa fa-user icon"></i>
+					</div>
+					<div className="input-box">
+						<select>
+							<option>01 jun</option>
+							<option>02 jun</option>
+							<option>03 jun</option>
+						</select>
+						<select>
+							<option>2020</option>
+							<option>2021</option>
+							<option>2022</option>
+						</select>
+					</div>
+				</div>
+				<div className="input-group">
+					<div className="input-box">
+						<button
+							className="pay-btn"
+							onClick={(e) => {
+								e.preventDefault();
+								console.log("order is succesfull");
+							}}
+						>
+							PAY NOW {totalPrice} BGN
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
 	);
 };
 
